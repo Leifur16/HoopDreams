8590e75891b6a1ee16f0b82a62ed83346a5cdb7f
Leifur16 <leifur16@ru.is>
2018-10-22 10:36:25 +0000

    inputs and type is reade exept for moment and basketballfieldstatus

--- HoopDreams/schema/types/basketballField.js
+++ HoopDreams/schema/types/basketballField.js
@@ -1,10 +1,9 @@
 //const Moment = require("../scalar");
+
 //console.log(Moment);
 
 /* TODO add: 
-yearOfCreation : Moment*
-• pickupGames An array of PickupGame (where the array cannot be null nor the
-items within the array)
+• yearOfCreation : Moment*
 • status: BasketballFieldStatus*
 */
 
@@ -13,6 +12,6 @@ module.exports = `
         id: ID!
         name: String!
         capacity: Int!
-
+        pickupGames: [PickupGame!]!
     }
 `;
--- HoopDreams/schema/types/pickupGame.js
+++ HoopDreams/schema/types/pickupGame.js
@@ -1,16 +1,14 @@
 /*
 TODO add:
-start : Moment*
+• start : Moment*
 • end : Moment*
-• location : BasketballField*
-• registeredPlayers : An array of Player (where the array cannot be null nor the
-items within the array)
-• host : Player*
 */
 
 module.exports = `
     type PickupGame {
         id: ID!
-
+        location: BasketballField!
+        registeredPlayers: [Player!]!
+        host: Player!
     }
 `;
