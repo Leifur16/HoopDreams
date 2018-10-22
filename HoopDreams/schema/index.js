1f3b0f9e9dc0ae548c94de2bfc545ed2d621a2c3
Leifur16 <leifur16@ru.is>
2018-10-22 15:09:01 +0000

    mutations ready

--- HoopDreams/schema/index.js
+++ HoopDreams/schema/index.js
@@ -1,9 +1,13 @@
 const types = require("./types");
 const enums = require("./enums");
 const queries = require("./queries");
+const inputs = require("./inputs");
+const mutations = require("./mutations");
 
 module.exports = `
     ${types}
     ${enums}
     ${queries}
+    ${inputs}
+    ${mutations}
 `;
